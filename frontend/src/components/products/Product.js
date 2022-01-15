c9a6d1a3fd4de0b8addbf028f9f204208a65b053
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-15 05:49:38 -0600

    Add product card content

--- frontend/src/components/products/Product.js
+++ frontend/src/components/products/Product.js
@@ -1,17 +1,27 @@
 import React from "react";
 
-import { Card, CardMedia } from "@mui/material";
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 
 const Product = (props) => {
+	const prod = props.product;
 	return (
 		<Card className="p-3 rounded" variant="outlined" elevation={0}>
-			<a href={`/products/${props.product._id}`}>
-				<CardMedia
-					component="img"
-					alt={`${props.product.name} image`}
-					image={`${props.product.image}`}
-				/>
+			<a href={`/products/${prod._id}`}>
+				<CardMedia component="img" alt={`${prod.name} image`} image={`${prod.image}`} />
 			</a>
+			<CardContent>
+				<a href={`/products/${prod._id}`} style={{ textDecoration: "none" }}>
+					<Typography gutterBottom variant="strong" component="div" color="text.primary">
+						{prod.name}
+					</Typography>
+				</a>
+				<div className="my-3">
+					<Typography variant="body2">
+						{prod.rating} from {prod.numReviews} reviews
+					</Typography>
+				</div>
+				<Typography variant="h4">${prod.price}</Typography>
+			</CardContent>
 		</Card>
 	);
 };
