044167c24d5c46cfe98d9bf9313c9703d40f26d4
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-16 05:30:05 -0600

    Add product details

--- frontend/src/components/products/ProductDetails.js
+++ frontend/src/components/products/ProductDetails.js
@@ -1,9 +1,50 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
+
+import Rating from "../UI/Rating";
+import products from "../../products";
+
+import { Grid, Stack, Paper, Divider } from "@mui/material";
 
 const ProductDetails = (props) => {
+	const params = useParams();
+	const prod = products.find((prod) => {
+		return prod._id === params.id;
+	});
+
 	return (
 		<div>
-			<h5>Product</h5>
+			<Link className="btn btn-light my-3" to="/">
+				Go Back
+			</Link>
+			<Grid container>
+				<Grid item md={6}>
+					<img
+						src={prod.image}
+						alt={prod.name}
+						style={{ height: "100%", width: "100%" }}
+					/>
+				</Grid>
+				<Grid item md={3}>
+					<Stack spacing={2} sx={{ pl: 4 }}>
+						<Paper elevation={0} sx={{ px: 2 }}>
+							<h3>{prod.name}</h3>
+						</Paper>
+						<Divider />
+						<Paper elevation={0} sx={{ px: 2 }}>
+							<Rating rating={prod.rating} text={`${prod.numReviews} reviews`} />
+						</Paper>
+						<Divider />
+						<Paper elevation={0} sx={{ px: 2 }}>
+							Price: ${prod.price}
+						</Paper>
+						<Divider />
+						<Paper elevation={0} sx={{ px: 2 }}>
+							Description: {prod.description}
+						</Paper>
+					</Stack>
+				</Grid>
+			</Grid>
 		</div>
 	);
 };
