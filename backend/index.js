c6da21c71e967f3685204e79919e45072b45bedb
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-16 15:46:41 -0600

    Add Express boilerplate

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+
+const express = require("express");
+const path = require("path");
+
+/* -------------------------------------------------------------------------- */
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
+/* -------------------------------------------------------------------------- */
+
+const productRoutes = require("./routes/product");
+
+app.get("/", (req, res) => {
+	res.send("API is running...");
+});
+
+app.use("/api/products", productRoutes);
+
+app.all("*", (req, res, next) => {
+	res.send("404 Not Found");
+});
+
+/* -------------------------------------------------------------------------- */
+
+const Port = process.env.PORT || 5000;
+
+app.listen(Port, () => {
+	console.log(`Mern Store is listening on port ${Port}`);
+});
