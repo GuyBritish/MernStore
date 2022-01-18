f88c0be377d21277849c6d5395592c6f62e34210
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-18 00:56:12 -0600

    Add error handler middleware

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,18 @@ app.get("/", (req, res) => {
 app.use("/api/products", productRoutes);
 
 app.all("*", (req, res, next) => {
-	res.send("404 Not Found");
+	const err = new Error(`Not Found - ${req.originalUrl}`);
+	res.status(404);
+	next(err);
+});
+
+app.use((err, req, res, next) => {
+	const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+	res.status(statusCode);
+	res.json({
+		message: err.message,
+		stack: process.env.NODE_ENV === "production" ? null : err.stack,
+	});
 });
 
 /* -------------------------------------------------------------------------- */
