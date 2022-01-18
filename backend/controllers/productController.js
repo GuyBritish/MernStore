11dcafd28018396f6f755dbabdd2f66adc65a646
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-18 00:57:54 -0600

    Add not found error

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -12,7 +12,8 @@ const getOneProduct = async (req, res) => {
 	if (product) {
 		res.json(product);
 	} else {
-		res.status(404).json({ message: "Product not found" });
+		res.status(404);
+		throw new Error("Product not found");
 	}
 };
 
