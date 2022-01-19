e5944f89b556bc7c9a4739ed8bc38bb77e1fb1d6
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-19 00:20:44 -0600

    Add product details action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -4,6 +4,9 @@ import {
 	PRODUCT_LIST_REQUEST,
 	PRODUCT_LIST_SUCCESS,
 	PRODUCT_LIST_FAIL,
+	PRODUCT_DETAILS_REQUEST,
+	PRODUCT_DETAILS_SUCCESS,
+	PRODUCT_DETAILS_FAIL,
 } from "../constants/productConst";
 
 export const listProducts = () => {
@@ -20,6 +23,29 @@ export const listProducts = () => {
 		} catch (err) {
 			dispatch({
 				type: PRODUCT_LIST_FAIL,
+				payload:
+					err.response && err.response.data.message
+						? err.response.data.message
+						: err.message,
+			});
+		}
+	};
+};
+
+export const listProductDetails = (id) => {
+	return async (dispatch) => {
+		try {
+			dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+			const options = {
+				url: `/api/products/${id}`,
+			};
+			const resp = await axios(options);
+
+			dispatch({ type: PRODUCT_DETAILS_SUCCESS, payload: resp.data });
+		} catch (err) {
+			dispatch({
+				type: PRODUCT_DETAILS_FAIL,
 				payload:
 					err.response && err.response.data.message
 						? err.response.data.message
