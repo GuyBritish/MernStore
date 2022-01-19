efd0304dd7cfb6d59abc33a60dba3566cdb08488
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-18 23:51:42 -0600

    Add loading spinner

---
+++ frontend/src/components/UI/Loader.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { CircularProgress } from "@mui/material";
+
+const Loader = () => {
+	return (
+		<React.Fragment>
+			<CircularProgress sx={{ width: 100, height: 100, margin: "auto", display: "block" }} />;
+			<span class="sr-only">Loading...</span>
+		</React.Fragment>
+	);
+};
+
+export default Loader;
