47c6c6cde60cf0a476ac3815bb5e299f6b956d80
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-20 00:39:35 -0600

    Add remove from cart reducer

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -23,7 +23,12 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
 				};
 			}
 		case CART_REMOVE_ITEM:
-			return state;
+			return {
+				...state,
+				cartItems: state.cartItems.filter((x) => {
+					return x.id !== action.payload;
+				}),
+			};
 		default:
 			return state;
 	}
