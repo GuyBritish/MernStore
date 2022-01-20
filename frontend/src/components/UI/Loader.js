7a8df739ef7060ad19be32bcd560227fb6d3973b
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-20 00:41:47 -0600

    Remove typo

--- frontend/src/components/UI/Loader.js
+++ frontend/src/components/UI/Loader.js
@@ -5,7 +5,7 @@ import { CircularProgress } from "@mui/material";
 const Loader = () => {
 	return (
 		<React.Fragment>
-			<CircularProgress sx={{ width: 100, height: 100, margin: "auto", display: "block" }} />;
+			<CircularProgress sx={{ width: 100, height: 100, margin: "auto", display: "block" }} />
 			<span className="sr-only">Loading...</span>
 		</React.Fragment>
 	);
