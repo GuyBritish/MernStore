88adab3ee0777da5a8a10696e1a51fa3f0ad1159
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-20 00:39:24 -0600

    Add remove from cart action

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { CART_ADD_ITEM } from "../constants/cartConst";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConst";
 
 export const addToCart = (id, qty) => {
 	return async (dispatch, getState) => {
@@ -19,6 +19,17 @@ export const addToCart = (id, qty) => {
 				countInStock: resp.data.countInStock,
 				qty,
 			},
+		});
+
+		localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+	};
+};
+
+export const removeFromCart = (id) => {
+	return async (dispatch, getState) => {
+		dispatch({
+			type: CART_REMOVE_ITEM,
+			payload: id,
 		});
 
 		localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
