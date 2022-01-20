d5f23af7df7807dc66ae9d4bd2ce744b6a904206
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-19 21:45:28 -0600

    Add cart reducer and local storage initial state

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,14 +3,22 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import { productDetailsReducer, productListReducer } from "../reducers/productReducer";
+import { cartReducer } from "../reducers/cartReducer";
 
 /* -------------------------------------------------------------------------- */
 
-const initialState = {};
+const initialState = {
+	cart: {
+		cartItems: localStorage.getItem("cartItems")
+			? JSON.parse(localStorage.getItem("cartItems"))
+			: [],
+	},
+};
 
 const reducer = combineReducers({
 	productList: productListReducer,
 	productDetails: productDetailsReducer,
+	cart: cartReducer,
 });
 
 const middleware = [thunk];
