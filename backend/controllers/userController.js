d5c322b042d09360445b9a204cfe8dc3ada72217
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-21 01:44:43 -0600

    Add get user profile controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -20,6 +20,21 @@ const authUser = async (req, res) => {
 	}
 };
 
+const getProfile = async (req, res) => {
+	const user = await User.findById(req.user._id);
+
+	if (user) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			isAdmin: user.isAdmin,
+		});
+	} else {
+		res.status(404);
+		throw new Error("User not found");
+	}
+};
+
 /* -------------------------------------------------------------------------- */
 
-module.exports = { authUser };
+module.exports = { authUser, getProfile };
