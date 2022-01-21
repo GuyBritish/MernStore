489563e7268bebad0b04444b57956550de801b51
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-21 00:36:27 -0600

    Add user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,23 @@
+const User = require("../models/userModel");
+
+const authUser = async (req, res) => {
+	const { email, password } = req.body;
+
+	const user = await User.findOne({ email });
+
+	if (user && (await user.matchPassword(password))) {
+		res.json({
+			_id: user._id,
+			name: user.name,
+			isAdmin: user.isAdmin,
+			token: null,
+		});
+	} else {
+		res.status(401);
+		throw new Error("Invalid email or password");
+	}
+};
+
+/* -------------------------------------------------------------------------- */
+
+module.exports = { authUser };
