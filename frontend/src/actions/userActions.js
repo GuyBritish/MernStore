f872d18d0fbaa0db880f75a5ecb1bead6d41befb
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-22 22:24:32 -0600

    Add user registration actions

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -4,6 +4,9 @@ import {
 	USER_LOGIN_FAIL,
 	USER_LOGIN_REQUEST,
 	USER_LOGIN_SUCCESS,
+	USER_REGISTER_REQUEST,
+	USER_REGISTER_SUCCESS,
+	USER_REGISTER_FAIL,
 	USER_LOGOUT,
 } from "../constants/userConst";
 
@@ -35,6 +38,49 @@ export const login = (email, password) => {
 		} catch (err) {
 			dispatch({
 				type: USER_LOGIN_FAIL,
+				payload:
+					err.response && err.response.data.message
+						? err.response.data.message
+						: err.message,
+			});
+		}
+	};
+};
+
+export const register = (name, email, password) => {
+	return async (dispatch) => {
+		try {
+			dispatch({ type: USER_REGISTER_REQUEST });
+
+			const options = {
+				url: "/api/users",
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				data: {
+					name,
+					email,
+					password,
+				},
+			};
+
+			const resp = await axios(options);
+
+			dispatch({
+				type: USER_REGISTER_SUCCESS,
+				payload: resp.data,
+			});
+
+			dispatch({
+				type: USER_LOGIN_SUCCESS,
+				payload: resp.data,
+			});
+
+			localStorage.setItem("userInfo", JSON.stringify(resp.data));
+		} catch (err) {
+			dispatch({
+				type: USER_REGISTER_FAIL,
 				payload:
 					err.response && err.response.data.message
 						? err.response.data.message
