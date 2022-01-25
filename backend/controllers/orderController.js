f9cd3861bf020cdf3fb7a00588a11a7ac8398650
Max <80323354+GuyBritish@users.noreply.github.com>
2022-01-24 21:55:22 -0600

    Add order controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,38 @@
+const Model = require("../models/orderModel");
+
+const addOrder = async (req, res) => {
+	// Needs backend verification
+	const {
+		items,
+		shippingAddress,
+		paymentMethod,
+		itemsPrice,
+		taxPrice,
+		shippingPrice,
+		totalPrice,
+	} = req.body;
+
+	if (items && items.length === 0) {
+		res.status(400);
+		throw new Error("No order items");
+	} else {
+		const order = new Order({
+			items,
+			user: req.user._id,
+			shippingAddress,
+			paymentMethod,
+			itemsPrice,
+			taxPrice,
+			shippingAddress,
+			totalPrice,
+		});
+
+		const newOrder = await order.save();
+
+		res.status(201).json(newOrder);
+	}
+};
+
+/* -------------------------------------------------------------------------- */
+
+module.exports = { addOrder };
