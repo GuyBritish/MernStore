5192a9b00d6ba816a4ef725d318e3d987f7805a9
Max <80323354+GuyBritish@users.noreply.github.com>
2022-02-01 02:27:09 -0600

    Add get order details action

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -4,6 +4,9 @@ import {
 	ORDER_CREATE_REQUEST,
 	ORDER_CREATE_SUCCESS,
 	ORDER_CREATE_FAIL,
+	ORDER_DETAILS_REQUEST,
+	ORDER_DETAILS_SUCCESS,
+	ORDER_DETAILS_FAIL,
 } from "../constants/orderConst";
 
 export const createOrder = (order) => {
@@ -18,7 +21,7 @@ export const createOrder = (order) => {
 			} = getState();
 
 			const options = {
-				url: "/api/order",
+				url: "/api/orders",
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
@@ -36,6 +39,43 @@ export const createOrder = (order) => {
 		} catch (err) {
 			dispatch({
 				type: ORDER_CREATE_FAIL,
+				payload:
+					err.response && err.response.data.message
+						? err.response.data.message
+						: err.message,
+			});
+		}
+	};
+};
+
+export const getOrderDetails = (id) => {
+	return async (dispatch, getState) => {
+		try {
+			dispatch({
+				type: ORDER_DETAILS_REQUEST,
+			});
+
+			const {
+				userAuth: { userInfo },
+			} = getState();
+
+			const options = {
+				url: `/api/orders/${id}`,
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${userInfo.token}`,
+				},
+			};
+
+			const resp = await axios(options);
+
+			dispatch({
+				type: ORDER_DETAILS_SUCCESS,
+				payload: resp.data,
+			});
+		} catch (err) {
+			dispatch({
+				type: ORDER_DETAILS_FAIL,
 				payload:
 					err.response && err.response.data.message
 						? err.response.data.message
