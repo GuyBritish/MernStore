377b9a76ee62c6db328511ccafb4fdc676196b9f
Max <80323354+GuyBritish@users.noreply.github.com>
2022-02-01 02:08:47 -0600

    Fix order route url

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
-app.use("/api/order", orderRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.all("*", (req, res, next) => {
 	const err = new Error(`Not Found - ${req.originalUrl}`);
