b4f34d95fef988a800a2614e817a0051a997aafa
Max <80323354+GuyBritish@users.noreply.github.com>
2022-02-26 16:09:08 -0600

    Add create and update product controllers

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -30,6 +30,48 @@ const deleteOneProduct = async (req, res) => {
 	}
 };
 
+const createProduct = async (req, res) => {
+	const product = new Product({
+		name: "Sample name",
+		price: 0,
+		user: req.user._id,
+		image: "/image/sample.jpg",
+		brand: "Sample brand",
+		category: "Sample category",
+		countInStock: 0,
+		numReviews: 0,
+		description: "Sample description",
+	});
+
+	const createdProduct = await product.save();
+
+	res.status(201).json(createdProduct);
+};
+
+const updateProduct = async (req, res) => {
+	const params = req.params;
+	const { name, price, image, brand, category, countInStock, numReviews, description } = req.body;
+
+	const product = await Product.findById(params.id);
+
+	if (product) {
+		product.name = name;
+		product.price = price;
+		product.image = image;
+		product.brand = brand;
+		product.category = category;
+		product.countInStock = countInStock;
+		product.numReviews = numReviews;
+		product.description = description;
+
+		const updatedProduct = await product.save();
+		res.status(201).json(updatedProduct);
+	} else {
+		res.status(404);
+		throw new Error("Product not found");
+	}
+};
+
 /* -------------------------------------------------------------------------- */
 
-module.exports = { getProducts, getOneProduct, deleteOneProduct };
+module.exports = { getProducts, getOneProduct, deleteOneProduct, createProduct, updateProduct };
