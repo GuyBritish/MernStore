da2d7b14d4de172f7aec29aee8ee8b225fbf4797
Max <80323354+GuyBritish@users.noreply.github.com>
2022-02-26 06:17:11 -0600

    Add admin's product delete action

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -7,6 +7,9 @@ import {
 	PRODUCT_DETAILS_REQUEST,
 	PRODUCT_DETAILS_SUCCESS,
 	PRODUCT_DETAILS_FAIL,
+	PRODUCT_DELETE_REQUEST,
+	PRODUCT_DELETE_SUCCESS,
+	PRODUCT_DELETE_FAIL,
 } from "../constants/productConst";
 
 export const listProducts = () => {
@@ -46,6 +49,38 @@ export const listProductDetails = (id) => {
 		} catch (err) {
 			dispatch({
 				type: PRODUCT_DETAILS_FAIL,
+				payload:
+					err.response && err.response.data.message
+						? err.response.data.message
+						: err.message,
+			});
+		}
+	};
+};
+
+export const deleteProduct = (id) => {
+	return async (dispatch, getState) => {
+		try {
+			dispatch({ type: PRODUCT_DELETE_REQUEST });
+
+			const {
+				userAuth: { userInfo },
+			} = getState();
+
+			const options = {
+				url: `/api/products/${id}`,
+				method: "DELETE",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${userInfo.token}`,
+				},
+			};
+			await axios(options);
+
+			dispatch({ type: PRODUCT_DELETE_SUCCESS });
+		} catch (err) {
+			dispatch({
+				type: PRODUCT_DELETE_FAIL,
 				payload:
 					err.response && err.response.data.message
 						? err.response.data.message
