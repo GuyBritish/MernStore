54eacc7e6ffbbcefd8f36462118fa043d2d653fa
Max <80323354+GuyBritish@users.noreply.github.com>
2022-04-17 00:27:01 -0500

    Fix api config

--- backend/config/media.js
+++ backend/config/media.js
@@ -2,11 +2,11 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 /* -------------------------------------------------------------------------- */
-
+console.log(process.env.CLOUDINARY_API_SECRET);
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.CLOUDINARY_API_SECRET,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const storage = new CloudinaryStorage({
