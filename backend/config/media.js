c975fbd2ad48d463f490ea0f6bfbd7585e4afe0c
Max <80323354+GuyBritish@users.noreply.github.com>
2022-04-27 22:54:59 -0500

    Remove debug print

--- backend/config/media.js
+++ backend/config/media.js
@@ -2,7 +2,6 @@ const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
 /* -------------------------------------------------------------------------- */
-console.log(process.env.CLOUDINARY_API_SECRET);
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
