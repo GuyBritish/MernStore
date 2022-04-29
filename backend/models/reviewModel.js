976bbe1a521880028fda9e80386d510d73cc6c8f
Max <80323354+GuyBritish@users.noreply.github.com>
2022-04-29 05:16:37 -0500

    Add user ref to product review model

--- backend/models/reviewModel.js
+++ backend/models/reviewModel.js
@@ -5,6 +5,11 @@ const ReviewSchema = new mongoose.Schema(
 		name: { type: String, required: true },
 		comment: { type: String, required: true },
 		rating: { type: Number, required: true },
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
 	},
 	{ timestamps: true }
 );
