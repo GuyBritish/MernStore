30826a835d7d6abf2b07d4df237395e1daf2e4b7
Max <80323354+GuyBritish@users.noreply.github.com>
2022-07-25 07:40:07 -0500

    Update sample data

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,7 +1,7 @@
 const products = [
 	{
 		name: "Airpods Wireless Bluetooth Headphones",
-		image: "https://res.cloudinary.com/dqttprqho/image/upload/v1650177552/MernStore/airpods_gldm36.jpg",
+		image: "https://res.cloudinary.com/dqttprqho/image/upload/v1658719112/MernStore/MV7N2_AV1_qnias2.jpg",
 		description:
 			"Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
 		brand: "Apple",
@@ -13,7 +13,7 @@ const products = [
 	},
 	{
 		name: "iPhone 11 Pro 256GB Memory",
-		image: "https://res.cloudinary.com/dqttprqho/image/upload/v1650177552/MernStore/phone_qx5j17.jpg",
+		image: "https://res.cloudinary.com/dqttprqho/image/upload/v1658719170/MernStore/iphone-11-pro-max-64gb-chinh-hang-moi-100-fullbox-chua-active-tbh-bac_cdvsaq.webp",
 		description:
 			"Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
 		brand: "Apple",
@@ -24,8 +24,20 @@ const products = [
 		numReviews: 8,
 	},
 	{
+		name: "iPhone 13 512GB Memory",
+		image: "https://res.cloudinary.com/dqttprqho/image/upload/v1658718941/MernStore/efc838be9e8aa13b4de4cbfc0b927fd9.iPhone_13_PDP_Position-1A_Color_PRODUCTRED__VN-692x692_riedit.jpg",
+		description:
+			"The iPhone 13 iterate upon the successful iPhone 12 with new cameras and longer battery life. The notch's size has been reduced, and the rear camera module now sits at a diagonal.",
+		brand: "Apple",
+		category: "Electronics",
+		price: 1129.99,
+		countInStock: 7,
+		rating: 4.5,
+		numReviews: 8,
+	},
+	{
 		name: "Cannon EOS 80D DSLR Camera",
-		image: "https://res.cloudinary.com/dqttprqho/image/upload/v1650177552/MernStore/camera_zswvyk.jpg",
+		image: "https://res.cloudinary.com/dqttprqho/image/upload/v1658719257/MernStore/canon-eos-80d-dslr-camera_zxdbrz.jpg",
 		description:
 			"Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
 		brand: "Cannon",
@@ -37,7 +49,7 @@ const products = [
 	},
 	{
 		name: "Sony Playstation 4 Pro White Version",
-		image: "https://res.cloudinary.com/dqttprqho/image/upload/v1650177552/MernStore/playstation_hneo3t.jpg",
+		image: "https://res.cloudinary.com/dqttprqho/image/upload/v1658719214/MernStore/61OJ13Gy-QL._SR720_720__ichjx3.jpg",
 		description:
 			"The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
 		brand: "Sony",
@@ -48,8 +60,20 @@ const products = [
 		numReviews: 12,
 	},
 	{
+		name: "Sony Playstation 5 White Version",
+		image: "https://res.cloudinary.com/dqttprqho/image/upload/v1658718772/MernStore/ps5-credit-sie_2000x1270_jh31nu.jpg",
+		description:
+			"The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
+		brand: "Sony",
+		category: "Electronics",
+		price: 599.99,
+		countInStock: 2,
+		rating: 5,
+		numReviews: 5,
+	},
+	{
 		name: "Logitech G-Series Gaming Mouse",
-		image: "https://res.cloudinary.com/dqttprqho/image/upload/v1650177552/MernStore/mouse_mmwmho.jpg",
+		image: "https://res.cloudinary.com/dqttprqho/image/upload/v1658719393/MernStore/logitech-g305-lightspeed-wireless-gaming-mouse-with-white-color-500x500_m0yd6k.jpg",
 		description:
 			"Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
 		brand: "Logitech",
@@ -58,6 +82,18 @@ const products = [
 		countInStock: 7,
 		rating: 3.5,
 		numReviews: 10,
+	},
+	{
+		name: "Razer Basilisk Ultimate - Wireless Gaming Mouse",
+		image: "https://res.cloudinary.com/dqttprqho/image/upload/v1658718783/MernStore/gearvn-chuot-razer-basilisk-ultimate-1_2d28d30423c8455fa7b5de74a612189f_x0hofs.jpg",
+		description:
+			"With the Razer Basilisk Ultimate, victory has never been this personal. As a high-performance wireless gaming mouse that can be tuned to look, play and feel exactly the way you want, the only people running out of options will be your opponents.",
+		brand: "Razer",
+		category: "Electronics",
+		price: 119.99,
+		countInStock: 5,
+		rating: 4.0,
+		numReviews: 6,
 	},
 	{
 		name: "Amazon Echo Dot 3rd Generation",
